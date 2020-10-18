471ce5d2889535121214f0bb2433150c5ff499fd
Jorgeonidas <zangetsujm@gmail.com>
2020-10-18 13:51:12 -0500

    first commit, git ignore, basic character, floor 4x4

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public LayerMask groundMask;
+
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private GameObject groundChecker;
+    private bool groundedPlayer;
+
+    [SerializeField]
+    private float playerSpeed = 2.0f;
+    [SerializeField]
+    private float jumpHeight = 1.0f;
+    [SerializeField]
+    private float gravityValue = -9.81f;
+    [SerializeField]
+    private float groundDistance = 0.005f;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        groundChecker = transform.GetChild(0).gameObject;
+    }
+
+    void Update()
+    {
+        groundedPlayer = Physics.CheckSphere(groundChecker.transform.position, groundDistance, groundMask, QueryTriggerInteraction.Ignore);
+        Debug.Log(groundedPlayer);
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
